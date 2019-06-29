d47739d6889845a049379df524dd109dd080f8c0
lbf2018 <306746541@qq.com>
2019-06-29 17:21:10 +0800

    继续智障

--- js/index.js
+++ js/index.js
@@ -1,9 +1,17 @@
-var f = (
-    function f() {
-        return 1;
-    },
-        function () {
-            return '2';
-        }
-)();
-console.log(typeof f);
+var str="你好123";
+var leng=0;
+for (let i = 0; i <str.length ; i++) {
+    if (str.charCodeAt(i)>255){
+        leng+=2;
+    } else {
+        leng++;
+    }
+}
+console.log(leng);
+FFF.prototype.aaa="aaa";
+function FFF() {
+    this.aaa="bbb";
+}
+var fff=new FFF();
+
+console.log();
